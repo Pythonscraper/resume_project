18731b0d67099d98302a759895871f15f392043f
Pythonscraper <babadjanovmahmudjon2002@gmail.com>
2022-05-06 11:56:55 +0500

    Django project

---
+++ web/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.core.mail import send_mail
+
+
+def home(request):
+    if request.method == 'POST':
+        name = request.POST.get('full-name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+
+        data = {
+            'name': name,
+            'email': email,
+            'subject': subject,
+            'message': message,
+        }
+
+        message = '''
+        New message: {}
+
+        From: {}
+        '''.format(data['message'], data['email'])
+        send_mail(data['subject'], message, '', ['babadjanovmahmudjon2002@gmail.com'])
+
+    return render(request, 'web/index.html')
